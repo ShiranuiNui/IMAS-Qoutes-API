73da5718ac1c43b3ccfafe52acbf8a63f3f885f3
ShiranuiNui <shiranuisayaka@gmail.com>
2018-07-11 00:56:29 +0900

    [Add] POCO Layer

---
+++ IMAS-QOTD-API.Server/Models/ProductionsEnum.cs
@@ -0,0 +1,10 @@
+/* fixformat ignore:start */
+namespace IMAS_QOTD_API.Model
+{
+    public enum ProductionsEnum{
+
+    }
+}
+/* fixformat ignore:end */
+
+//765AS 961 876 346 765MS 315 283
---
+++ IMAS-QOTD-API.Server/Models/Quote.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+namespace IMAS_QOTD_API.Model
+{
+    public class Qoute
+    {
+        public string QouteString { get; set; }
+        public string Speaker { get; set; }
+        public string Production { get; set; }
+    }
+}
